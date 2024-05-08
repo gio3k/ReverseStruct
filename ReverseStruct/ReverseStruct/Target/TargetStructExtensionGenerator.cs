866e6648d2869192d70efbafba901d60b127747f
Gio <lotuspar0@gmail.com>
2024-05-09 01:57:41 +0800

    Comments and code cleaning

--- ReverseStruct/ReverseStruct/Target/TargetStructExtensionGenerator.cs
+++ ReverseStruct/ReverseStruct/Target/TargetStructExtensionGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using ReverseStruct.StaticCode.Crumbs;
 
@@ -8,12 +7,18 @@ namespace ReverseStruct.Target;
 
 public static class TargetStructExtensionGenerator
 {
-	public static readonly string ExtensionClassName = "StructExtensions";
+	public const string ExtensionClassName = "StructExtensions";
 
+	/// <summary>
+	/// Generate source for the "Reverse(this ref *struct*)" method body
+	/// </summary>
+	/// <param name="targetStructInfo">TargetStructInfo</param>
+	/// <returns>Source</returns>
+	/// <exception cref="ArgumentOutOfRangeException">Field has invalid reversal method</exception>
 	[MethodImpl( MethodImplOptions.AggressiveInlining )]
 	private static string GenerateReverseExtRefMethodBody( TargetStructInfo targetStructInfo )
 	{
-		var result = $"\t// {targetStructInfo.Fields.Count} field(s)";
+		var result = $"// {targetStructInfo.Fields.Count} field(s)";
 		foreach ( var (name, reversalMethod) in targetStructInfo.Fields )
 		{
 			result += reversalMethod switch
@@ -23,32 +28,41 @@ public static class TargetStructExtensionGenerator
 				                                    			x.{name} = BinaryPrimitives.ReverseEndianness(x.{name});
 				                                    """,
 				ReversalMethod.ExistingExtension => $"""
-				                                    
-				                                    			x.{name}.Reverse();
-				                                    """,
+				                                     
+				                                     			x.{name}.Reverse();
+				                                     """,
 				_ => throw new ArgumentOutOfRangeException()
 			};
 		}
+
 		return result;
 	}
 
+	/// <summary>
+	/// Generate additional source for the partial static extension class
+	/// </summary>
+	/// <param name="targetStructInfo">TargetStructInfo</param>
+	/// <returns>Source</returns>
 	[MethodImpl( MethodImplOptions.AggressiveInlining )]
-	private static string GenerateSource( TargetStructInfo targetStructInfo )
-	{
-		return $@"{FileCrumbs.Header}
+	private static string GenerateSource( TargetStructInfo targetStructInfo ) =>
+		$@"{FileCrumbs.Header}
 using System;
 using System.Buffers.Binary;
 {NamespaceCrumbs.PublicNamespaceStatementStarter} {{
     public static partial class {ExtensionClassName} {{
 		/* Generated extension code for {targetStructInfo.TargetShortName} */
 		public static void Reverse(this ref {targetStructInfo.TargetFullName} x) {{
-		{GenerateReverseExtRefMethodBody( targetStructInfo )}
+			{GenerateReverseExtRefMethodBody( targetStructInfo )}
 		}}
 	}}
 }}
 ";
-	}
 
+	/// <summary>
+	/// Add another part of the extension class for reversal of the target struct
+	/// </summary>
+	/// <param name="ctx">SourceProductionContext</param>
+	/// <param name="v">TargetStructInfo</param>
 	public static void GenerateExtensionPartialForTargetStruct( SourceProductionContext ctx, TargetStructInfo? v )
 	{
 		if ( v is not { } targetStructInfo )
--- ReverseStruct/ReverseStruct/Target/TargetStructInfo.cs
+++ ReverseStruct/ReverseStruct/Target/TargetStructInfo.cs
@@ -1,7 +1,12 @@
 using System.Collections.Generic;
+using ReverseStruct.StaticCode;
 
 namespace ReverseStruct.Target;
 
+/// <summary>
+/// Information about a struct with the [Reversible] attribute
+/// <see cref="ReversibleAttributeDefinition"/>
+/// </summary>
 public record struct TargetStructInfo
 {
 	public string TargetFullName;
